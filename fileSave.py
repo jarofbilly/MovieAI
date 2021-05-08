ee61301b6d81802ce61ef8ca6eb81b377cc49f07
RequiredRedundancy <60747146+RequiredRedundancy@users.noreply.github.com>
2021-05-08 16:44:42 +0100

    Add files via upload
    After saving the file to the machine in queestion the array can be loaded again directly instead of having to recompute it every time, saving on processing power.

---
+++ fileSave.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+movie_data = pd.read_csv('C:/Users/Dominic/Downloads/archive/movies_metadata.csv',low_memory=False)
+movie_data['overview'] = movie_data['overview'].fillna('')
+file = open("lk","rb")
+
+#Can save the array with file = open("fileName","wb") then np.save(file)
+
+sim_matrix = np.load(file)
+file.close
+indices = pd.Series(movie_data.index, index=movie_data['title']).drop_duplicates()
+indices[:10]
+
+def content_based_recommender(title, sim_scores = sim_matrix):
+
+    idx = indices[title]
+
+    sim_scores = list(enumerate(sim_matrix[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_scores = sim_scores[1:11]
+
+    movie_indices = [i[0] for i in sim_scores]
+    results = movie_data['title'].iloc[movie_indices]
+    #sortedOrder = results.sort_values(['sim_scores'], ascending = False)
+    # sortedOrder
+    return results
